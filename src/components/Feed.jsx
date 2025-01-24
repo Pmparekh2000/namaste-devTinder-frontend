e383c76b15cce4228887baf31e3ce3994748a9b0
Pmparekh2000 <pmp.tech2000@gmail.com>
2025-01-24 12:05:15 +0530

    Added feeds API and feeds slice to app

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,4 +1,28 @@
+import axios from "axios";
+import { BASE_URL, USER } from "../utils/constants";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addFeed } from "../utils/feedSlice";
+
 const Feed = () => {
+  const feed = useSelector((store) => store.feed);
+  const dispatch = useDispatch();
+
+  const getUserFeed = async () => {
+    // If the feed is already present then don't make the API call instead return
+    if (feed) return;
+    try {
+      const userFeedData = await axios.get(BASE_URL + USER + "/feed", {
+        withCredentials: true,
+      });
+      dispatch(addFeed(userFeedData?.data));
+    } catch (error) {
+      console.log("Error obtained while making the feed API call", error);
+    }
+  };
+  useEffect(() => {
+    getUserFeed();
+  }, []);
   return <div>This is the feed component</div>;
 };
 

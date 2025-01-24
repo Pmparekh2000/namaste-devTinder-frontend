e6cd74aa2a6a20087a896baa871d7aacc3c01bbe
Pmparekh2000 <pmp.tech2000@gmail.com>
2025-01-24 06:47:04 +0530

    Preventing logout on refresh and handling logout

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,36 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
+import axios from "axios";
+import { BASE_URL, PROFILE } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser, removeUser } from "../utils/userSlice";
+import { useEffect } from "react";
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userData = useSelector((store) => store.user);
+  // Trying to fetch user profile if the JWT token is already set
+  const fetchUserProfile = async () => {
+    try {
+      const response = await axios.get(BASE_URL + PROFILE + "/view", {
+        withCredentials: true,
+      });
+      dispatch(addUser(response.data.user));
+    } catch (error) {
+      if (error.status === 401) {
+        dispatch(removeUser());
+        navigate("/login");
+      }
+      console.log("Error obtained while making the profile API call", error);
+    }
+  };
+  useEffect(() => {
+    if (userData === null) {
+      fetchUserProfile();
+    }
+  }, []);
   return (
     <div>
       <NavBar />
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,38 @@
-import { useSelector } from "react-redux";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { AUTH, BASE_URL } from "../utils/constants";
+import { removeUser } from "../utils/userSlice";
+import { Link, useNavigate } from "react-router-dom";
 
 const NavBar = () => {
   const user = useSelector((store) => store.user);
-  console.log("user obtained is", user?.photoUrl);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      const response = await axios.post(
+        BASE_URL + AUTH + "/logout",
+        {},
+        {
+          withCredentials: true,
+        }
+      );
+      console.log("response obtained is", response);
+
+      dispatch(removeUser());
+      navigate("/login");
+    } catch (error) {
+      console.log("Error obtained while making the logout API call", error);
+    }
+  };
 
   return (
     <div className="navbar bg-base-300">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">🚀 DevTinder</a>
+        <Link to="/" className="btn btn-ghost text-xl">
+          🚀 DevTinder
+        </Link>
       </div>
       {user && (
         <div className="flex-none gap-2">
@@ -27,16 +52,16 @@ const NavBar = () => {
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
             >
               <li>
-                <a className="justify-between">
+                <Link to="/profile" className="justify-between">
                   Profile
                   <span className="badge">New</span>
-                </a>
+                </Link>
               </li>
               <li>
                 <a>Settings</a>
               </li>
               <li>
-                <a>Logout</a>
+                <a onClick={() => handleLogout()}>Logout</a>
               </li>
             </ul>
           </div>
